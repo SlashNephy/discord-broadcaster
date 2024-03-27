d165a7a10e5bca21efa14ebe9659d8f74bd9d626
SlashNephy <spica@starry.blue>
2024-03-27 23:35:18 +0900

    remove emit log

--- web/controller/events.go
+++ web/controller/events.go
@@ -12,13 +12,15 @@ import (
 )
 
 func (co *Controller) GetEvents(c echo.Context) error {
+	ctx := c.Request().Context()
+
 	rawTopics := c.QueryParam("topics")
 	if rawTopics == "" {
 		return echo.NewHTTPError(http.StatusBadRequest, "No topics specified")
 	}
 
 	topics := mapset.NewSet(strings.Split(rawTopics, ",")...)
-	slog.InfoContext(c.Request().Context(), "connected", slog.Any("topics", topics))
+	slog.InfoContext(ctx, "connected", slog.Any("topics", topics))
 
 	// write HTTP headers
 	c.Response().Header().Set("Content-Type", "text/event-stream")
@@ -27,18 +29,17 @@ func (co *Controller) GetEvents(c echo.Context) error {
 
 	events := make(chan *entity.EventFrame, 1)
 	defer close(events)
-	co.usecase.SubscribeEvent(c.Request().Context(), topics, events)
+	co.usecase.SubscribeEvent(ctx, topics, events)
 
 	for {
 		select {
-		case <-c.Request().Context().Done():
+		case <-ctx.Done():
 			return nil
 		case event := <-events:
 			if _, err := c.Response().Write([]byte(event.String())); err != nil {
 				return err
 			}
 
-			slog.DebugContext(c.Request().Context(), "emit", slog.Any("frame", event))
 			c.Response().Flush()
 		}
 	}
